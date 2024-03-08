1d3a87fb5c7e068d973eb6ec38cc6bc13a483d91
ZUHAD UL HADI <75843975+chzuhadulhadi@users.noreply.github.com>
2024-03-08 15:00:59 +0500

    dynamic routing

---
+++ pages/posts/[id].js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+
+const MyComponent=({ id }) => {
+    return (
+        <div>
+            <h1>Component with ID: {id}</h1>
+            {/* Add your component content here */}
+        </div>
+    );
+};
+
+export default MyComponent;
